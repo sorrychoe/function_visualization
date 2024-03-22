451d7bc38223412a399a779040d4d5aa353484fc
sorrychoe <cjssoote@gmail.com>
2024-03-22 17:01:49 +0900

    add two graph

--- cubic.m
+++ cubic.m
@@ -1,10 +1,10 @@
 x = -100:100;
-y = 2*x+6;
+y = 0.6*x.^3 + 2*x.^2-3*x+1;
 plot(x,y);
 
 ax = gca;
 ax.XAxisLocation = "origin";
 ax.YAxisLocation = "origin";
 
-xlim([-20,20]);
-ylim([-20,20]);
+xlim([-30,30]);
+ylim([-30,30]);
